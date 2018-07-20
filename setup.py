97cb22b658e2cecc58d90b5db4265d51db956828
dmkskn <dmksknn@gmail.com>
2018-07-20 18:15:15 +0300

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,35 @@
-#!/usr/bin/env python3
+import sys
 from setuptools import setup
+
+assert sys.version_info >= (3, 6, 0), 'meduza requires Python 3.6+'
+from pathlib import Path
+
+
+BASE = Path(__file__).parent
+
+def get_long_description() -> str:
+    with open(BASE / 'README.md', 'r', encoding='utf-8') as f:
+        return f.read()
 
 
 setup(
     name='meduza',    
-    version='1.0.1',  
+    version='18.7',
     license='MIT',
-    description='A simple Python module that wraps the https://meduza.io API.',
+    author='Dima Koskin',
+    author_email='dmksknn@gmail.com',
+    description='A simple Python module that wraps the meduza.io API.',
+    long_description=get_long_description(),
+    long_description_content_type='text/markdown',
+    python_requires='>=3.6',
     url="https://github.com/dmkskn/meduza",
     py_modules=['meduza'],
+    keywords='meduza media wrapper russia english russian',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
     )
