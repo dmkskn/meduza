0298703cfe779df7142ad15baa4ad1c6eadebf22
dmkskn <dmksknn@gmail.com>
2018-07-21 15:28:10 +0300

    cleanup

--- meduza.py
+++ meduza.py
@@ -10,50 +10,84 @@ from urllib.request import urlopen as _urlopen
 from itertools import islice as _islice
 
 
-__all__ = ['LANGUAGES', 'EN_SECTIONS', 'RU_SECTIONS', 'EN_TAGS', 'RU_TAGS',
-           'stocks', 'get', 'section', 'tag', 'reactions_for',
-           'iter_reactions_for', 'latest_push', 'is_today']
+__all__ = [
+    "LANGUAGES",
+    "EN_SECTIONS",
+    "RU_SECTIONS",
+    "EN_TAGS",
+    "RU_TAGS",
+    "stocks",
+    "get",
+    "section",
+    "tag",
+    "reactions_for",
+    "iter_reactions_for",
+    "latest_push",
+    "is_today",
+]
 
 
-# Constants 
-LANGUAGES = ("ru", "en")
+LANGUAGES = [
+    "ru", 
+    "en",
+]
 
-EN_SECTIONS = ("news")
+EN_SECTIONS = [
+    "news",
+]
 
-RU_SECTIONS = ("news", "articles", "shapito", "razbor", "games", "podcasts")
+RU_SECTIONS = [
+    "news", 
+    "articles", 
+    "shapito", 
+    "razbor", 
+    "games", 
+    "podcasts",
+]
 
-EN_TAGS = ("news", "like it or not", "games")
+EN_TAGS = [
+    "news", 
+    "like it or not", 
+    "games",
+]
 
-RU_TAGS = ("новости", "истории", "разбор", "шапито", "игры", "подкасты",
-           "партнерский материал")
+RU_TAGS = [
+    "новости",
+    "истории",
+    "разбор",
+    "шапито",
+    "игры",
+    "подкасты",
+    "партнерский материал",
+]
 
 
-_BASEURL = 'https://meduza.io'
-_API_SUFFIX = 'api/v3'
-_SEARCH_API = f'{_BASEURL}/{_API_SUFFIX}/search?'
-_SOCIAL_API = f'{_BASEURL}/{_API_SUFFIX}/social?'
-_STOCK_API = f'{_BASEURL}/{_API_SUFFIX}/stock/all'
-_LATEST_PUSH = f'{_BASEURL}/{_API_SUFFIX}/push/chrome/latest'
+
+_BASEURL = "https://meduza.io"
+_API_SUFFIX = "api/v3"
+_SEARCH_API = f"{_BASEURL}/{_API_SUFFIX}/search?"
+_SOCIAL_API = f"{_BASEURL}/{_API_SUFFIX}/social?"
+_STOCK_API = f"{_BASEURL}/{_API_SUFFIX}/stock/all"
+_LATEST_PUSH = f"{_BASEURL}/{_API_SUFFIX}/push/chrome/latest"
 
 
 # maps tags to sections (russian)
 _rus_section_from = {
-    'новости': 'news',
-    'истории': 'articles',
-    'шапито': 'shapito',
-    'разбор': 'razbor',
-    'игры': 'games',
-    'подкасты': 'podcasts',
-    'партнерский материал': 'news',
-    }
-
+    "новости": "news",
+    "истории": "articles",
+    "шапито": "shapito",
+    "разбор": "razbor",
+    "игры": "games",
+    "подкасты": "podcasts",
+    "партнерский материал": "news",
+}
 
 # maps tags to sections (english)
 _eng_section_from = {
-    'news' : 'news',
-    'games': 'news',
-    'like it or not': 'news',
-    }
+    "news": "news",
+    "games": "news",
+    "like it or not": "news",
+}
 
 
 # open URL and return JSON response (as dict)
@@ -64,10 +98,10 @@ def _urlopenjson(url):
     data = response.read()
     # if the data is compressed using gzip, then decompress this.
     # (u is a gzip file if the first two bytes are '0x1f' and '0x8b')
-    if headers.get('Content-Encoding') == 'gzip':
+    if headers.get("Content-Encoding") == "gzip":
         data = _gzip.decompress(data)
     # remove all non-breaking spaces
-    data = data.decode('utf-8').replace('\xa0', ' ')
+    data = data.decode("utf-8").replace("\xa0", " ")
     return _json.loads(data)
 
 
@@ -87,12 +121,12 @@ def get(url: str, as_dict=False) -> dict:
     if not url.startswith(_BASEURL):
         url = _urljoin(_BASEURL, url)
     if _API_SUFFIX not in url:
-        url = url.replace(_BASEURL, f'{_BASEURL}/{_API_SUFFIX}')
-    json_as_dict = _urlopenjson(url)['root']
+        url = url.replace(_BASEURL, f"{_BASEURL}/{_API_SUFFIX}")
+    json_as_dict = _urlopenjson(url)["root"]
     return json_as_dict if as_dict else Article(json_as_dict)
 
 
-def section(section: str, results=24, language='ru', as_dict=False):
+def section(section: str, results=24, language="ru", as_dict=False):
     """Gets articles from the `section`.
 
     `section` - Section name (see `meduza.EN_SECTIONS` and q
@@ -101,15 +135,15 @@ def section(section: str, results=24, language='ru', as_dict=False):
     `language` - Russian or English version of meduza.io (see 
     `meduza.LANGUAGES`)."""
     page = 0
-    while results and page <= 10: # (more pages are not available)
+    while results and page <= 10:  # (more pages are not available)
         payload = {
-            'chrono': section,
-            'locale': language, 
-            'page': page, 
-            'per_page': '24'
-            }
+            "chrono": section,
+            "locale": language,
+            "page": page,
+            "per_page": "24",
+        }
         url = _SEARCH_API + _urlencode(payload)
-        for full_dict_url in _urlopenjson(url)['documents'].keys():
+        for full_dict_url in _urlopenjson(url)["documents"].keys():
             if results:
                 yield get(full_dict_url, as_dict=as_dict)
                 results -= 1
@@ -118,7 +152,7 @@ def section(section: str, results=24, language='ru', as_dict=False):
         page += 1
 
 
-def tag(tag, results=24, language='ru', as_dict=False):
+def tag(tag, results=24, language="ru", as_dict=False):
     """Gets articles with the `tag`.
 
     `tag` - An article tag. Same as in `article['tag']['name']` (see
@@ -126,13 +160,13 @@ def tag(tag, results=24, language='ru', as_dict=False):
     `results`  -- How many articles to return;
     `language` -- Russian or English version of meduza.io (see 
     `meduza.LANGUAGES`)."""
-    # choose a section 
-    if language == 'ru':
+    # choose a section
+    if language == "ru":
         section_ = _rus_section_from[tag]
     else:
         section_ = _eng_section_from[tag]
-    # key function 
-    valid_tag = lambda a: a['tag']['name'] == tag
+    # key function
+    valid_tag = lambda a: a["tag"]["name"] == tag
     # generator of the current section `section_`
     # `results=24*10` because probably this is the maximum value
     section_generator = section(section_, 24 * 10, language, as_dict=as_dict)
@@ -149,79 +183,78 @@ def reactions_for(article: dict) -> dict:
 def iter_reactions_for(*articles):
     """Gets number of reactions in social networks (and number of 
     comments on meduza.io) for all articles."""
-    urls = (a['url'] for a in articles)
-    url = _SOCIAL_API + _urlencode({'links':_json.dumps([*urls])})
+    urls = (a["url"] for a in articles)
+    url = _SOCIAL_API + _urlencode({"links": _json.dumps([*urls])})
     dirty_dict = _urlopenjson(url)
-    result = (dirty_dict[url]['stats'] for url in dirty_dict)
+    result = (dirty_dict[url]["stats"] for url in dirty_dict)
     return result
 
 
 def latest_push() -> dict:
     """Gets the latest push."""
-    push = _urlopenjson(_LATEST_PUSH)['notification']
-    return {'title': push['title'], 'body': push['body'], 'url': push['url']}
+    push = _urlopenjson(_LATEST_PUSH)["notification"]
+    return {"title": push["title"], "body": push["body"], "url": push["url"]}
 
 
 def is_today(article: dict) -> bool:
     """Defines whether the `article` is published today.
 
     `article` - an article dict."""
-    return _date.today() == _date.fromtimestamp(article['published_at'])
+    return _date.today() == _date.fromtimestamp(article["published_at"])
 
 
-
-class Article: # BETA
+class Article:  # BETA
     def __init__(self, info: dict):
         self._info = info
-        self.url = _BASEURL + info['url']
-        self.title = info['title']
-        self.second_title = info.get('second_title')
-        self.description = info['description']
-        self.source = info['source']
+        self.url = _BASEURL + info["url"]
+        self.title = info["title"]
+        self.second_title = info.get("second_title")
+        self.description = info["description"]
+        self.source = info["source"]
         self.reactions = reactions_for(info)
-        self.tag = info['tag']['name']
-        self.type = info['document_type']
-        self.footer = info['footer']
-        self.is_blocks = 'blocks' in info['content'] or 'slides' in info['content']
-        self.is_html = 'body' in info['content']
+        self.tag = info["tag"]["name"]
+        self.type = info["document_type"]
+        self.footer = info["footer"]
+        self.is_blocks = "blocks" in info["content"] or "slides" in info["content"]
+        self.is_html = "body" in info["content"]
         self.image = self._get_image(info)
         self.blocks = self._get_blocks_or_none(info)
         self.html = self._get_html_or_none(info)
         self.datetime = {
-            'modification': _datetime.fromtimestamp(info['modified_at']),
-            'publication': _datetime.fromtimestamp(info['published_at']),
-        } 
-        
+            "modification": _datetime.fromtimestamp(info["modified_at"]),
+            "publication": _datetime.fromtimestamp(info["published_at"]),
+        }
+
     def _get_image(self, info):
-        if info.get('image'):
+        if info.get("image"):
             return {
-                'url': _BASEURL + info['image'].get('large_url') or \
-                info['image'].get('small_url'),
-                'caption': info['image']['caption'],
-                'credit': info['image']['credit']
-                }
+                "url": _BASEURL + info["image"].get("large_url")
+                or info["image"].get("small_url"),
+                "caption": info["image"]["caption"],
+                "credit": info["image"]["credit"],
+            }
         else:
             return None
-    
+
     def _get_blocks_or_none(self, info):
         if self.is_blocks:
-            return info['content'].get('blocks') or info['content'].get('slides')
+            return info["content"].get("blocks") or info["content"].get("slides")
         else:
             return None
-    
+
     def _get_html_or_none(self, info):
         if self.is_html:
-            return info['content']['body']
+            return info["content"]["body"]
         else:
             return None
 
     def __repr__(self):
         return f"<{self.tag.title()}: '{self.title}'>"
-    
+
     def __iter__(self):
-        iters = dict((x,y) for x,y in self.__dict__.items() if not x.startswith('_'))
+        iters = dict((x, y) for x, y in self.__dict__.items() if not x.startswith("_"))
         for x, y in iters.items():
             yield x, y
-    
+
     def as_dict(self):
         return self._info
