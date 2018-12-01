56e1b28c18e1a5a301de3413a7073ff9e38e8d5a
dmkskn <dmksknn@gmail.com>
2018-12-01 11:21:23 +0300

    18.12.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def get_long_description() -> str:
 
 setup(
     name='meduza',    
-    version='18.10.5',
+    version='18.12.1',
     license='MIT',
     author='Dima Koskin',
     author_email='dmksknn@gmail.com',
