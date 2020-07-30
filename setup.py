adbd88a942b18c71abe32c9cb914432bab71b327
dmkskn <dmksknn@gmail.com>
2020-07-30 10:19:29 +0300

    Update setup.py version to 20.7.2

--- setup.py
+++ setup.py
@@ -1,35 +1,36 @@
 import sys
 from setuptools import setup
 
-assert sys.version_info >= (3, 6, 0), 'meduza requires Python 3.6+'
+assert sys.version_info >= (3, 6, 0), "meduza requires Python 3.6+"
 from pathlib import Path
 
 
 BASE = Path(__file__).parent
 
+
 def get_long_description() -> str:
-    with open(BASE / 'README.md', 'r', encoding='utf-8') as f:
+    with open(BASE / "README.md", "r", encoding="utf-8") as f:
         return f.read()
 
 
 setup(
-    name='meduza',    
-    version='20.7.1',
-    license='MIT',
-    author='Dima Koskin',
-    author_email='dmksknn@gmail.com',
-    description='A simple Python module that wraps the meduza.io API.',
+    name="meduza",
+    version="20.7.2",
+    license="MIT",
+    author="Dima Koskin",
+    author_email="dmksknn@gmail.com",
+    description="A simple Python module that wraps the meduza.io API.",
     long_description=get_long_description(),
-    long_description_content_type='text/markdown',
-    python_requires='>=3.6',
+    long_description_content_type="text/markdown",
+    python_requires=">=3.6",
     url="https://github.com/dmkskn/meduza",
-    py_modules=['meduza'],
-    keywords='meduza media wrapper russia english russian',
+    py_modules=["meduza"],
+    keywords="meduza media wrapper russia english russian",
     classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3 :: Only',
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3 :: Only",
     ],
-    )
+)
